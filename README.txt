243dfea305a9e4d477a9c25c1bc09ed015214ad9
Matthew Gilliard <matthew.gilliard@hp.com>
2015-09-29 15:18:57 +0100

    First bit of UI is visible

--- README.txt
+++ README.txt
@@ -1,7 +1,14 @@
+Playing around with clojurescript, node & ncurses
+
 Download and run:
 
+  lein npm install
   lein cljsbuild once
 
 then
 
   node target/pow.js
+
+
+(nb I think the above should be collapsable
+ into a single line somehow)
--- src/pow/pow.cljs
+++ src/pow/pow.cljs
@@ -3,9 +3,23 @@
 
 (nodejs/enable-util-print!)
 
-(println "Hello from Node!")
-
 (defn -main [& args]
-  (println "Hello world!"))
+  (let [blessed (nodejs/require "blessed")
+        screen (.screen blessed #js {:smartCSR true})
+        box (.box blessed #js {:top "center" :left "center"
+                               :width "50%" :height "50%"
+                               :content "HELLO {bold}WORLD{/bold}"
+                               :tags true
+                               :border #js {:type "line"}
+                               :style #js {:fg "#ffffff"
+                                           :bg "#666666"
+                                           :border #js {:fg "#f0f0f0"}}})]
+
+    (aset screen "title" "POW!")
+
+    (.append screen box)
+    (.focus box)
+
+    (.render screen)))
 
 (set! *main-cli-fn* -main)
